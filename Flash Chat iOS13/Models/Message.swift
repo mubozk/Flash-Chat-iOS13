35c37eba9e8718e7b5e54b3c4261e782326d03bb
Muhammet Bozkurt <mubozk@icloud.com>
2023-04-24 06:37:04 +0300

    Implement TableView
    TableView implementation with custom cells using a UITableViewCell an specifying it to look like a messaging app

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Muhammet BOZKURT on 24.04.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct Message {
+    let sender: String
+    let body: String
+}
